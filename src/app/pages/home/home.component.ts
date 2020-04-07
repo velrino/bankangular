9e0645e93cfb5071aa3502ee80f47eff63bfab51
velrino <denismagalhaesvelrino@gmail.com>
2020-04-07 20:24:08 -0300

    deixando barra de progresso dinamica

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  data: any = {
+    aberto: 1000,
+    disponivel: 3000,
+    fechado: 1660,
+    total: 0
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.calcularTotal();
   }
 
+  calcularTotal() {
+    const { aberto, disponivel, fechado } = this.data
+    this.data.total = (aberto+disponivel+fechado)
+  }
+
+  calcularPorcentagem(valor: number = 0) {
+    let porcentagem = (valor / this.data.total) * 100
+    return Math.round(porcentagem * 10) / 10
+  }
 }
